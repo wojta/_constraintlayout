d4721e1a66bb0e263e49f8dd85e97c48126befdf
Vojtěch Sázel <vojtech@sazel.cz>
2017-06-21 10:39:29 +0200

    ConstraintLayout removed to check the build

--- app/src/main/java/cz/sazel/android/constraintlayout/MainActivity.java
+++ app/src/main/java/cz/sazel/android/constraintlayout/MainActivity.java
@@ -2,12 +2,17 @@ package cz.sazel.android.constraintlayout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvHello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvHello = (TextView) findViewById(R.id.tvHello);
+        findViewById(R.id.btJ8Click).setOnClickListener(v -> tvHello.setText(getText(R.string.j8text)));
     }
 }
